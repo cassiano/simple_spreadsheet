5212d1b6649f9fbee15811a61d2049c4f0aa76b1
Cassiano D'Andrea <cassiano@tagview.com.br>
2015-10-22 00:19:35 -0200

    Renaming `find_or_create_cell_by_ref` to `find_or_create_cell` and creating first real unit tests

--- simple_spreadsheet.rb
+++ simple_spreadsheet.rb
@@ -1,4 +1,4 @@
-DEBUG = true
+DEBUG = false
 
 require 'set'
 
@@ -10,148 +10,157 @@ class Spreadsheet
   end
 
   # def set(ref, content = nil)
-  #   cell = find_or_create_cell_by_ref(ref)
+  #   cell = find_or_create_cell(ref)
   #
   #   cell.content = content
   # end
 
-  def find_or_create_cell_by_ref(ref)
+  def find_or_create_cell(ref, content = nil)
     ref = ref.upcase.to_sym
 
-    cells[ref] || add_cell(ref)
+    (cells[ref] || add_cell(ref)).tap do |cell|
+      cell.content = content if content
+    end
   end
 
   def add_cell(ref, content = nil)
+    raise "Cell #{ref} already exists" if cells[ref]
+
     ref = ref.upcase.to_sym
 
     Cell.new(self, ref, content).tap do |cell|
       cells[ref] = cell
     end
   end
-end
 
-class Cell
-  CELL_REF_REG_EXP = /[A-Z]+\d+/i
-  DEFAULT_VALUE    = 0
+  class Cell
+    CELL_REF_REG_EXP = /[A-Z]+\d+/i
+    DEFAULT_VALUE    = 0
 
-  attr_reader :spreadsheet, :ref, :references, :observers, :content
+    # List of possible exceptions.
+    class CircularReferenceError < StandardError; end
 
-  def initialize(spreadsheet, ref, content = nil)
-    puts "Creating cell #{ref}" if DEBUG
+    attr_reader :spreadsheet, :ref, :references, :observers, :content
 
-    ref = ref.upcase.to_sym
+    def initialize(spreadsheet, ref, content = nil)
+      puts "Creating cell #{ref}" if DEBUG
 
-    @spreadsheet = spreadsheet
-    @ref         = ref
-    @references  = Set.new
-    @observers   = Set.new
+      ref = ref.upcase.to_sym
 
-    self.content = content
-  end
+      @spreadsheet = spreadsheet
+      @ref         = ref
+      @references  = Set.new
+      @observers   = Set.new
 
-  def add_observer(cell)
-    puts "Adding observer #{cell.ref} to #{ref}" if DEBUG
-
-    observers << cell
-  end
-
-  def remove_observer(cell)
-    puts "Removing observer #{cell.ref} from #{ref}" if DEBUG
-
-    observers.delete cell
-  end
-
-  def content=(new_content)
-    old_references = Set.new
-    new_references = Set.new
-
-    old_references = references.clone if is_formula?
-
-    # Notice this may change the value returned by method `is_formula?`.
-    @content = String === new_content ? new_content.strip : new_content
-
-    if is_formula?
-      new_references = content[1..-1].scan(CELL_REF_REG_EXP).inject Set.new do |memo, ref|
-        memo << spreadsheet.find_or_create_cell_by_ref(ref)
-      end
+      self.content = content
     end
 
-    add_references    new_references - old_references
-    remove_references old_references - new_references
+    def add_observer(cell)
+      puts "Adding observer #{cell.ref} to #{ref}" if DEBUG
 
-    eval true
-  end
+      observers << cell
+    end
 
-  def eval(reevaluate = false)
-    previous_content = @evaluated_content
+    def remove_observer(cell)
+      puts "Removing observer #{cell.ref} from #{ref}" if DEBUG
 
-    @evaluated_content = nil if reevaluate
+      observers.delete cell
+    end
 
-    @evaluated_content ||=
+    def content=(new_content)
+      old_references = Set.new
+      new_references = Set.new
+
+      old_references = references.clone if is_formula?
+
+      # Notice this may change the value returned by method `is_formula?`.
+      @content = String === new_content ? new_content.strip : new_content
+
       if is_formula?
-        puts ">>> Calculating formula for #{self.ref}" if DEBUG
+        new_references = content[1..-1].scan(CELL_REF_REG_EXP).inject Set.new do |memo, ref|
+          memo << spreadsheet.find_or_create_cell(ref)
+        end
+      end
 
-        evaluated_content = content[1..-1]
+      add_references    new_references - old_references
+      remove_references old_references - new_references
 
-        references.each do |cell|
-          evaluated_content.gsub! cell.ref.to_s, cell.eval.to_s
+      eval true
+    end
+
+    def eval(reevaluate = false)
+      previous_content = @evaluated_content
+
+      @evaluated_content = nil if reevaluate
+
+      @evaluated_content ||=
+        if is_formula?
+          puts ">>> Calculating formula for #{self.ref}" if DEBUG
+
+          evaluated_content = content[1..-1]
+
+          references.each do |cell|
+            evaluated_content.gsub! cell.ref.to_s, cell.eval.to_s
+          end
+
+          Kernel.eval evaluated_content
+        else
+          content
         end
 
-        Kernel.eval evaluated_content
-      else
-        content
+      # Fire all observers if evaluated content has changed.
+      fire_observers if previous_content != @evaluated_content
+
+      @evaluated_content || DEFAULT_VALUE
+    end
+
+    def directly_or_indirectly_references?(cell)
+      references.include?(cell) || references.any? { |reference| reference.directly_or_indirectly_references?(cell) }
+    end
+
+    private
+
+    def fire_observers
+      puts "Firing #{ref}'s observers" if DEBUG && observers.any?
+
+      observers.each do |cell|
+        cell.eval true
+      end
+    end
+
+    def add_reference(cell)
+      if cell.directly_or_indirectly_references?(self)
+        raise CircularReferenceError, "Circular reference detected when adding reference #{cell.ref} to #{ref}!"
       end
 
-    # Fire all observers if evaluated content has changed.
-    fire_observers if previous_content != @evaluated_content
+      puts "Adding reference #{cell.ref} to #{ref}" if DEBUG
 
-    @evaluated_content || DEFAULT_VALUE
-  end
-
-  def directly_or_indirectly_references?(cell)
-    references.include?(cell) || references.any? { |reference| reference.directly_or_indirectly_references?(cell) }
-  end
-
-  private
-
-  def fire_observers
-    puts "Firing #{ref}'s observers" if DEBUG && observers.any?
-
-    observers.each do |cell|
-      cell.eval true
+      references << cell
+      cell.add_observer self
     end
-  end
 
-  def add_reference(cell)
-    raise "Circular reference detected when adding reference #{cell.ref} to #{ref}!" if cell.directly_or_indirectly_references?(self)
+    def remove_reference(cell)
+      puts "Removing reference #{cell.ref} from #{ref}" if DEBUG
 
-    puts "Adding reference #{cell.ref} to #{ref}" if DEBUG
-
-    references << cell
-    cell.add_observer self
-  end
-
-  def remove_reference(cell)
-    puts "Removing reference #{cell.ref} from #{ref}" if DEBUG
-
-    references.delete cell
-    cell.remove_observer self
-  end
-
-  def add_references(cells)
-    cells.each do |cell|
-      add_reference cell
+      references.delete cell
+      cell.remove_observer self
     end
-  end
 
-  def remove_references(cells)
-    cells.each do |cell|
-      remove_reference cell
+    def add_references(cells)
+      cells.each do |cell|
+        add_reference cell
+      end
     end
-  end
 
-  def is_formula?
-    String === content && content[0] == '='
+    def remove_references(cells)
+      cells.each do |cell|
+        remove_reference cell
+      end
+    end
+
+    def is_formula?
+      String === content && content[0] == '='
+    end
   end
 end
 
@@ -172,7 +181,7 @@ spreadsheet = Spreadsheet.new
 # c1.content = '= (A2 + B1) ** 2'
 # puts a1.observers.map(&:ref).inspect
 # puts b1.observers.map(&:ref).inspect
-# b2 = spreadsheet.find_or_create_cell_by_ref(:a2)
+# b2 = spreadsheet.find_or_create_cell(:a2)
 # puts b2.observers.map(&:ref).inspect
 # puts c1.content
 # puts c1.eval
@@ -187,21 +196,21 @@ spreadsheet = Spreadsheet.new
 # puts c1.eval
 # puts d1.eval
 
-a1 = spreadsheet.add_cell :A1, 10
-b1 = spreadsheet.add_cell :B1, '= A1 + 1'
-c1 = spreadsheet.add_cell :C1, '= B1 + 2'
-d1 = spreadsheet.add_cell :D1, '= C1 + 3'
-e1 = spreadsheet.add_cell :E1, '       = D1 + 4        '
-
-puts e1.eval
-
-a1.content = 20
-puts e1.eval
-
-c1.content = 100
-puts e1.eval
-
-c1.content = '= B1 + 200'
-puts e1.eval
-
-a1.content = '= F1 * G1 ** E1 - 10'
+# a1 = spreadsheet.add_cell :A1, 10
+# b1 = spreadsheet.add_cell :B1, '= A1 + 1'
+# c1 = spreadsheet.add_cell :C1, '= B1 + 2'
+# d1 = spreadsheet.add_cell :D1, '= C1 + 3'
+# e1 = spreadsheet.add_cell :E1, '       = D1 + 4        '
+#
+# puts e1.eval
+#
+# a1.content = 20
+# puts e1.eval
+#
+# c1.content = 100
+# puts e1.eval
+#
+# c1.content = '= B1 + 200'
+# puts e1.eval
+#
+# a1.content = '= F1 * G1 ** E1 - 10'
--- test/test_simple_spreadsheet.rb
+++ test/test_simple_spreadsheet.rb
@@ -2,21 +2,122 @@ require_relative '../simple_spreadsheet'
 require 'contest'
 require 'turn/autorun'
 
-class TestSpreadSheet < Test::Unit::TestCase
+class TestSpreadsheet < Test::Unit::TestCase
   context 'SpreadSheet' do
     setup do
-      @spreadsheet = SpreadSheet.new
+      @spreadsheet = Spreadsheet.new
+    end
+
+    test 'new and empty spreadsheets have no cells' do
+      assert_equal 0, @spreadsheet.cells.count
+    end
+
+    test '#find_or_create_cell finds preexistent cells' do
+      a1 = @spreadsheet.add_cell :A1
+
+      assert_equal a1, @spreadsheet.find_or_create_cell(:A1)
+    end
+
+    test '#find_or_create_cell creates new cells as needed' do
+      a1 = @spreadsheet.find_or_create_cell(:A1)
+
+      assert_equal :A1, a1.ref
+    end
+
+    test 'cells can be found using symbol or string case insensitive references' do
+      a1 = @spreadsheet.add_cell :A1
+
+      assert_equal a1, @spreadsheet.find_or_create_cell(:A1)
+      assert_equal a1, @spreadsheet.find_or_create_cell(:a1)
+      assert_equal a1, @spreadsheet.find_or_create_cell('A1')
+      assert_equal a1, @spreadsheet.find_or_create_cell('a1')
     end
 
     context 'Cell' do
-      setup do
-        @developer = ["Cassiano D'Andrea <cassiano.dandrea@tagview.com.br>", Time.now.utc, '-0300']
+      test 'can hold scalar values, like numbers and strings' do
+        contents = {
+          a1: 1,
+          a2: 1.234,
+          a3: 'Some Text'
+        }
+
+        a1 = @spreadsheet.add_cell :A1, contents[:a1]
+        a2 = @spreadsheet.add_cell :A2, contents[:a2]
+        a3 = @spreadsheet.add_cell :A3, contents[:a3]
+
+        assert_equal contents[:a1], a1.eval
+        assert_equal contents[:a2], a2.eval
+        assert_equal contents[:a3], a3.eval
       end
 
-      test '#add_cell' do
-        a1 = spreadsheet.add_cell :A1, 10
+      test 'can hold formulas, referencing other cells' do
+        contents = {
+          a1: 1,
+          a2: 2,
+          a3: '= (A1 + A2) * 3'
+        }
 
-        ...
+        a1 = @spreadsheet.add_cell :A1, contents[:a1]
+        a2 = @spreadsheet.add_cell :A2, contents[:a2]
+        a3 = @spreadsheet.add_cell :A3, contents[:a3]
+
+        assert_equal contents[:a1], a1.eval
+        assert_equal contents[:a2], a2.eval
+        assert_equal (contents[:a1] + contents[:a2]) * 3, a3.eval
+      end
+
+      test 'can hold formulas, given referencing are not circular' do
+        contents = {
+          a1: '= A2',
+          a2: '= A3',
+          a3: '= A4',
+          a4: '= A5',
+          a5: '= A1'
+        }
+
+        assert_nothing_raised do
+          a1 = @spreadsheet.find_or_create_cell :A1, contents[:a1]
+          a2 = @spreadsheet.find_or_create_cell :A2, contents[:a2]
+          a3 = @spreadsheet.find_or_create_cell :A3, contents[:a3]
+          a4 = @spreadsheet.find_or_create_cell :A4, contents[:a4]
+        end
+
+        assert_raises Spreadsheet::Cell::CircularReferenceError do
+          a5 = @spreadsheet.find_or_create_cell :A5, contents[:a5]
+        end
+      end
+
+      test 'have default values' do
+        a1 = @spreadsheet.add_cell :A1
+
+        assert_equal Spreadsheet::Cell::DEFAULT_VALUE, a1.eval
+      end
+
+      test 'have empty references and observers when created' do
+        a1 = @spreadsheet.add_cell :A1
+
+        assert_equal 0, a1.references.count
+        assert_equal 0, a1.observers.count
+      end
+
+      test 'saves references to other cells' do
+        a1 = @spreadsheet.add_cell :A1, '= A2'
+        a2 = @spreadsheet.find_or_create_cell :A2
+
+        references = Set.new
+        references << a2
+
+        assert_equal references, a1.references
+      end
+
+      test 'are marked as observers in other cells when referencing them' do
+        a1 = @spreadsheet.add_cell :A1, '= A2'
+        a2 = @spreadsheet.find_or_create_cell :A2
+
+        observers = Set.new
+        observers << a1
+
+        assert_equal observers, a2.observers
       end
     end
   end
